0d54141e11809052ac0368a75448ec551baca691
Louis Payette <payette1996@gmail.com>
2023-03-21 15:13:03 -0400

    initial commit

---
+++ public/client.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script src="public/js/script.js"></script>
+    <title>Client</title>
+</head>
+<body>
+    <div class="wrapper">
+        <h1>Client</h1>
+    </div>
+</body>
+</html>
